b0e2a520180167d372e997b4d76a2d3bad94507d
Peter Harrison <pmc.harrison@gmail.com>
2018-09-17 11:32:09 +0100

    Incremental write out to csv

--- analyses/examples.m
+++ analyses/examples.m
@@ -7,7 +7,7 @@ params.leman2000_local_decay_sec = 0.1;
 params.leman2000_global_decay_sec = 1.5;
 params.collins2014_use_closure = false;
    
-input_dir = 'C:\Users\Peter\Documents\MATLAB\matlab-expectation-models\media\example-wav-files';
+input_dir = 'C:\Users\Peter\Documents\MATLAB\matlab-expectation-models\media\example-wav-file';
 input_file = 'id=1_genre=classical_c-id=1009_e-id=75_ic_category_1.wav';
 
 % [leman, collins, collins_detail] = analyse_sequence(input_file, input_dir, params, jlmtpath);
